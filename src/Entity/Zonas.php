921c0d6b2ee86d0d0038e742384e9a52c4c1b506
jbernaldiaz <jbernaldiaz@gmail.com>
2020-12-15 22:44:21 -0300

    Creacion de tablas y entidades User y zonas

---
+++ src/Entity/Zonas.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ZonasRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ZonasRepository::class)
+ */
+class Zonas
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $zona;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getZona(): ?string
+    {
+        return $this->zona;
+    }
+
+    public function setZona(string $zona): self
+    {
+        $this->zona = $zona;
+
+        return $this;
+    }
+}
